3fbe0022cc5503abbd3851158c63d8cb54fa0a8a
adelooney66 <adrian.looney@gmail.com>
2021-04-10 20:14:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# slack-clone
+TODO 
+
+Create read me! 
+
+Slack clone created using React.js.........
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBhyOfgYjCKaCCCZWuCVA3wJVOmwGpm7Ms",
+    authDomain: "slack-clone-bd940.firebaseapp.com",
+    projectId: "slack-clone-bd940",
+    storageBucket: "slack-clone-bd940.appspot.com",
+    messagingSenderId: "682545251041",
+    appId: "1:682545251041:web:0fe51d711459d99a4a6da0",
+    measurementId: "G-RQCR5KD104"
+  };
